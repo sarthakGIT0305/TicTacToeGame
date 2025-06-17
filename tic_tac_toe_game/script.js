a03b20ac7100df56004b73a01e7a71611193f6ca
sarthak gupta <sarthakgupta0305@gmail.com>
2025-06-17 19:25:39 +0530

    added js file

--- tic_tac_toe_game/script.js
+++ tic_tac_toe_game/script.js
@@ -1,1 +1,9 @@
- 
+console.log("welcome to tic tac toe");
+let bg_music = new Audio("/assets/bgmusic.mp3");
+let move_music = new Audio("/assets/move-audio.mp3");
+let turn = "X"; 
+
+changeTurn = ()=>{
+    return turn === "X" ? "O": "X"
+}
+
